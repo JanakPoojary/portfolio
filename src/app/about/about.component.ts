93173ace51d10328e5383fa68af15afd558dabf2
JanakPoojary <janakrajpoojari@gmail.com>
2022-02-16 20:47:44 +0530

    Adds basic feature components and styles

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+  about: string = "I'm a software developer with 1.5+ year of experience in full stack web development. I am currently working with frameworks like Asp.net Core and Angular. I have also worked with machine learning library like Tensorflow.js on real world application. I’m also familiar with Node.js, Express.js, JavaScript, HTML, CSS, Python, jQuery, Postgres, SQL but I’m always adding new skills to my repertoire. I’m eager to meet other software engineers. Let's connect!";
+  name: string = "JANAK";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
